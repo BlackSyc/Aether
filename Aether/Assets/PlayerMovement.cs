63ffc4831f00bdb47f576785c11a213363934247
Syc <marcvandeuren@gmail.com>
2020-02-29 14:07:11 +0100

    Implemented basic movement for a cube using character controller and input system

---
+++ Aether/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 5;
+
+    private CharacterController characterController;
+    private Vector2 movementInput;
+
+
+    void Awake()
+    {
+        this.characterController = this.GetComponent<CharacterController>();
+    }
+
+    // Gets called from PlayerInput when the movement action is triggered.
+    // Contains a Vector2 for the movement direction.
+    public void Move(CallbackContext context)
+    {
+        this.movementInput = context.ReadValue<Vector2>();
+    }
+
+    void Update()
+    {
+        Vector3 localMovement = transform.right * movementInput.x + transform.forward * movementInput.y;
+        characterController.Move(localMovement * movementSpeed * Time.deltaTime);
+    }
+}
