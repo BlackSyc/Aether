dcf72c9c03f8e6daf89c54478c47b6c40e446041
Syc <marcvandeuren@gmail.com>
2020-03-01 14:39:55 +0100

    Implemented puzzle1 triggers

---
+++ Aether/Assets/Scripts/Puzzle1/Puzzle1_Child.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1_Child : MonoBehaviour
+{
+    public bool IsTriggered { get; private set; }
+
+    [SerializeField]
+    private Puzzle1_Manager puzzleManager;
+
+    [SerializeField]
+    private Material triggeredMaterial;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Puzzle1_Trigger") && !IsTriggered)
+        {
+            Debug.Log("Triggered one puzzlepiece!");
+            IsTriggered = true;
+            GetComponent<MeshRenderer>().material = triggeredMaterial;
+            puzzleManager.CheckPuzzleComplete();
+        }
+    }
+}
---
+++ Aether/Assets/Scripts/Puzzle1/Puzzle1_Manager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle1_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Puzzle1_Child> puzzlePieces;
+
+    [SerializeField]
+    private Material puzzleCompleteMaterial;
+
+    public void CheckPuzzleComplete()
+    {
+        if(puzzlePieces.TrueForAll(x => x.IsTriggered))
+        {
+            GetComponent<MeshRenderer>().material = puzzleCompleteMaterial;
+            Debug.Log("Puzzle1 completed!");
+        }
+    }
+}
