e0f696cb409068bbb648c8b47d206ff0efef7189
Syc <marcvandeuren@gmail.com>
2020-03-02 20:09:20 +0100

    Implemented casting system and arcane missile spell with debug lines.

---
+++ Aether/Assets/Scripts/Spell System/SpellObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpellObject : MonoBehaviour
+{
+    public abstract void CastStarted();
+
+    public abstract void CastInterrupted();
+
+    public abstract void CastCanceled();
+
+    public abstract void CastFired();
+
+}
---
+++ Aether/Assets/Scripts/Spell System/Spells/ArcaneMissile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcaneMissile : SpellObject
+{
+    public override void CastCanceled()
+    {
+        Debug.Log("Arcane Missile Cancelled!");
+    }
+
+    public override void CastStarted()
+    {
+        Debug.Log("Arcane Missile Cast started!");
+    }
+
+    public override void CastFired()
+    {
+        Debug.Log("Arcane Missile Fired!");
+    }
+
+    public override void CastInterrupted()
+    {
+        Debug.Log("Arcane Missile Interrupted!");
+    }
+}
