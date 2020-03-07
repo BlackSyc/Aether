1e6aa96979c5c196f86e0121fcf483db5daff39f
Syc <marcvandeuren@gmail.com>
2020-03-07 02:01:53 +0100

    Added cloak missiles

--- Aether/Assets/Scripts/Player/Cloak.cs
+++ Aether/Assets/Scripts/Player/Cloak.cs
@@ -7,10 +7,15 @@ public class Cloak : MonoBehaviour
     [SerializeField]
     private CloakInfo cloakInfo;
 
+    [SerializeField]
+    private Spell missileSpell;
+
     public void Equip()
     {
         Debug.Log("Equipped cloak: " + cloakInfo.Name);
         cloakInfo.State.Equipped = true;
+        transform.parent.parent.GetComponent<SpellSystem>().SpellSlot1.SelectSpell(missileSpell);
+
         // add spells
         // perform spawning animation
     }
---
+++ Aether/Assets/Scripts/Spell System/Spells/NightmareMissile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightmareMissile : ArcaneMissile
+{
+    protected override bool Hit()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, .5f, Spell.layerMask);
+        if (colliders.Length > 0)
+        {
+            // Add damage and object removal logic
+
+            GetComponent<Animator>().SetTrigger("CastHit");
+            return true;
+        }
+        return false;
+    }
+}
