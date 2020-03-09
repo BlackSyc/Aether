fa0b92cf8dd3df65f14a0653b79f2db0e48fa032
Syc <marcvandeuren@gmail.com>
2020-03-09 19:34:23 +0100

    Added testrunner

---
+++ Aether/Assets/Scripts/AetherEvents/Testrunner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testrunner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(InvokeNextFrame(ThrowCloakSpawningEvents));
+    }
+
+    private void ThrowCloakSpawningEvents()
+    {
+        AetherEvents.GameEvents.Puzzle1Events.CompleteStage2();
+    }
+
+    private IEnumerator InvokeNextFrame(Action action)
+    {
+        yield return null;
+        action.Invoke();
+    }
+
+}
