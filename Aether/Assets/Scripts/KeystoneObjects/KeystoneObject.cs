05625bc3fea4ddeddd91343084867e8988ec29c4
Syc <marcvandeuren@gmail.com>
2020-03-29 00:33:44 +0100

    Added keystone object in dream level 1 and fixed crosshair bug

---
+++ Aether/Assets/Scripts/KeystoneObjects/KeystoneObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeystoneObject : MonoBehaviour
+{
+    [SerializeField]
+    private Keystone keystone;
+
+    public void PickUp()
+    {
+        Player.Instance.Inventory.PickupKeystone(keystone);
+        Destroy(gameObject);
+    }
+}
