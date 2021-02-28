a92a67c985dc5edfd05e3c11d014933ab9e767d6
Marc van Deuren <marcvandeuren@gmail.com>
2021-02-28 15:36:23 +0100

    working movement (laggy?)

---
+++ Aether/Assets/Scripts/Core/LocalObject.cs
@@ -0,0 +1,15 @@
+﻿using MLAPI;
+using UnityEngine;
+
+public class LocalObject : MonoBehaviour
+{
+    [SerializeField] private NetworkedBehaviour owner;
+
+    private void Start()
+    {
+        if (!owner.IsLocalPlayer)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
